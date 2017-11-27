df97fa12b9ba3c532eb69ff181b846c9178cae55
Miguel Angel Ortiz <ortiz.garcia.miguel.angel@gmail.com>
2017-11-27 16:55:18 +0100

    Logica del programa

--- app/src/main/java/xyz/maog/contador/MainActivity.java
+++ app/src/main/java/xyz/maog/contador/MainActivity.java
@@ -2,12 +2,43 @@ package xyz.maog.contador;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    public int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contador = 0;
     }
+
+    public void sumaContador(View vista) {
+        contador++;
+
+        mostrarResultado();
+    }
+
+    public void restaContador(View vista) {
+        contador--;
+
+        mostrarResultado();
+    }
+
+    public void reseteaContador(View vista) {
+        contador = 0;
+
+        mostrarResultado();
+    }
+
+    public void mostrarResultado() {
+        TextView textoResultado = (TextView) findViewById(R.id.TVcontador);
+
+        textoResultado.setText("Contador: " + contador);
+    }
+
 }
