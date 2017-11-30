03f496b68a91f83f0b6f7a7ee688fa704df23255
Miguel Angel Ortiz <ortiz.garcia.miguel.angel@gmail.com>
2017-11-30 18:37:47 +0100

    Añadida parte logica de un input para resetear al numero elegido y un checkbox para permitir el conteo negativo

--- app/src/main/java/xyz/maog/contador/MainActivity.java
+++ app/src/main/java/xyz/maog/contador/MainActivity.java
@@ -3,42 +3,54 @@ package xyz.maog.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public int contador;
+    TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textoResultado = (TextView) findViewById(R.id.TVcontadorN);
         contador = 0;
     }
 
     public void sumaContador(View vista) {
         contador++;
 
-        mostrarResultado();
+        textoResultado.setText("" + contador);
     }
 
     public void restaContador(View vista) {
         contador--;
 
-        mostrarResultado();
-    }
+        if (contador < 0) {
+            CheckBox negativos = (CheckBox) findViewById(R.id.CBnegativos);
 
-    public void reseteaContador(View vista) {
-        contador = 0;
-
-        mostrarResultado();
-    }
-
-    public void mostrarResultado() {
-        TextView textoResultado = (TextView) findViewById(R.id.TVcontadorN);
+            if (!negativos.isChecked()) {
+                contador = 0;
+            }
+        }
 
         textoResultado.setText("" + contador);
     }
 
+    public void reseteaContador(View vista) {
+        EditText numero_reset = (EditText) findViewById(R.id.ETresetN);
+
+        try {
+            contador = Integer.parseInt(numero_reset.getText().toString());
+        } catch (Exception e) {
+            contador = 0;
+        }
+
+        numero_reset.setText("");
+        textoResultado.setText("" + contador);
+    }
 }
